8f1f41146b2edc9c9cbbfb5377955982bd72e64d
Rex-Prime <itsrex.dev@gmail.com>
2026-05-24 20:23:43 +0500

    cord.nvim: customized the tooltips a bit

--- init.lua
+++ init.lua
@@ -334,7 +334,7 @@ nixInfo.lze.load({
     after = function(plugin)
       require("cord").setup({
         -- log_level = nil,
-        display ={
+        display = {
           theme = "void",
         },
         assets = {
@@ -343,7 +343,7 @@ nixInfo.lze.load({
           },
           nix = {
             tooltip = "❄️",
-          }
+          },
         },
         buttons = {
           {
@@ -355,10 +355,23 @@ nixInfo.lze.load({
             end,
           },
         },
+        advanced = {
+          plugin = {
+            autocmds = true,
+            cursor_update = "on_hold",
+            match_in_mappings = true,
+            debounce = {
+              delay = 50,
+              interval = 750,
+            },
+          },
+        },
         hooks = {
           post_activity = function(opts, activity)
             local version = vim.version()
-            activity.assets.small_text = string.format("Neovim %s.%s.%s", version.major, version.minor, version.patch)
+            activity.assets.small_text =
+              string.format("Neovim %s.%s.%s", version.major, version.minor, version.patch, opts.cursor_line)
+            activity.state = string.format("%s | %d:%d",opts.workspace, opts.cursor_line, opts.cursor_char)
           end,
         },
       })
